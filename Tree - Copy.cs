d3b6cac85a7decd6924a95fff842db0c2e155617
Lorenzo Usai <UsaiL@StPaulsSchool.org.uk>
2021-03-13 20:35:57 +0000

    done

--- Tree - Copy.cs
+++ Tree - Copy.cs
@@ -3,57 +3,21 @@ using System.Collections.Generic;
 using System.Text;
 namespace LinkedLists
 {
-    class Tree
-    { 
-        public class TNode
+    class Graph
+    {
+        class Vertex<T>
         {
-            public TNode NextL;
-            public TNode NextR;
-            public int data;
-            public TNode (int data = default)
-            {
-                this.data = data;
-            }
+            private int index;
+            private T data;
         }
-        private List<TNode> positions = new List<TNode>();
-        private TNode root = new TNode();
-        public Tree(int[] numbers)
+
+        class Graph<T>
         {
-            foreach (int i in numbers)
-            {
-                TNode current;
-                TNode newTNode  = new TNode(i); 
-                current = root;
-                if (root.data == default)
-                {
-                    root.data = i;
-                }
-                else
-                {
-                    do
-                    {
-                        if (current.data > i)
-                        {
-                            if (current.NextL == null)
-                            {
-                                current.NextL = newTNode;
-                                break;
-                            }
-                            current = current.NextL;
-                        }
-                        else
-                        {
-                            if (current.NextR == null)
-                            {
-                                current.NextR = newTNode;
-                                break;
-                            }
-                            current = current.NextR;
-                        }
-                    } while (current.data != i);
-                }
-            }
+            List<Vertex<T>> vertices;
+            bool[][] edges = new bool[][];
         }
+       
+             
         public bool BinarySearch(int Nemo)
         {
             TNode Dory = root;
